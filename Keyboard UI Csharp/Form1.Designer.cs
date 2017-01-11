8de6e387b4ff4ee389ef61b598272704b44ebc79
Alex <alexander1.richardson@gmail.com>
2017-01-11 18:34:37 +0000

    First switch to C#. RGB sliders implimented alongside backend code

---
+++ Keyboard UI Csharp/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Keyboard_UI_Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBarRed = new System.Windows.Forms.TrackBar();
+            this.labelRed = new System.Windows.Forms.Label();
+            this.numericUpDownRed = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownGreen = new System.Windows.Forms.NumericUpDown();
+            this.labelGreen = new System.Windows.Forms.Label();
+            this.trackBarGreen = new System.Windows.Forms.TrackBar();
+            this.numericUpDownBlue = new System.Windows.Forms.NumericUpDown();
+            this.labelBlue = new System.Windows.Forms.Label();
+            this.trackBarBlue = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBarRed
+            // 
+            this.trackBarRed.Location = new System.Drawing.Point(128, 49);
+            this.trackBarRed.Maximum = 255;
+            this.trackBarRed.Name = "trackBarRed";
+            this.trackBarRed.Size = new System.Drawing.Size(104, 45);
+            this.trackBarRed.TabIndex = 0;
+            this.trackBarRed.Scroll += new System.EventHandler(this.trackBarRed_Scroll);
+            // 
+            // labelRed
+            // 
+            this.labelRed.AutoSize = true;
+            this.labelRed.Location = new System.Drawing.Point(83, 49);
+            this.labelRed.Name = "labelRed";
+            this.labelRed.Size = new System.Drawing.Size(15, 13);
+            this.labelRed.TabIndex = 1;
+            this.labelRed.Text = "R";
+            this.labelRed.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDownRed
+            // 
+            this.numericUpDownRed.Location = new System.Drawing.Point(260, 49);
+            this.numericUpDownRed.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownRed.Name = "numericUpDownRed";
+            this.numericUpDownRed.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownRed.TabIndex = 2;
+            this.numericUpDownRed.ValueChanged += new System.EventHandler(this.numericUpDownRed_ValueChanged);
+            // 
+            // numericUpDownGreen
+            // 
+            this.numericUpDownGreen.Location = new System.Drawing.Point(260, 100);
+            this.numericUpDownGreen.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownGreen.Name = "numericUpDownGreen";
+            this.numericUpDownGreen.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownGreen.TabIndex = 5;
+            this.numericUpDownGreen.ValueChanged += new System.EventHandler(this.numericUpDownGreen_ValueChanged);
+            // 
+            // labelGreen
+            // 
+            this.labelGreen.AutoSize = true;
+            this.labelGreen.Location = new System.Drawing.Point(83, 100);
+            this.labelGreen.Name = "labelGreen";
+            this.labelGreen.Size = new System.Drawing.Size(15, 13);
+            this.labelGreen.TabIndex = 4;
+            this.labelGreen.Text = "G";
+            this.labelGreen.Click += new System.EventHandler(this.labelGreen_Click);
+            // 
+            // trackBarGreen
+            // 
+            this.trackBarGreen.Location = new System.Drawing.Point(128, 100);
+            this.trackBarGreen.Maximum = 255;
+            this.trackBarGreen.Name = "trackBarGreen";
+            this.trackBarGreen.Size = new System.Drawing.Size(104, 45);
+            this.trackBarGreen.TabIndex = 3;
+            this.trackBarGreen.Scroll += new System.EventHandler(this.trackBarGreen_Scroll);
+            // 
+            // numericUpDownBlue
+            // 
+            this.numericUpDownBlue.Location = new System.Drawing.Point(260, 151);
+            this.numericUpDownBlue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownBlue.Name = "numericUpDownBlue";
+            this.numericUpDownBlue.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownBlue.TabIndex = 8;
+            this.numericUpDownBlue.ValueChanged += new System.EventHandler(this.numericUpDownBlue_ValueChanged);
+            // 
+            // labelBlue
+            // 
+            this.labelBlue.AutoSize = true;
+            this.labelBlue.Location = new System.Drawing.Point(83, 151);
+            this.labelBlue.Name = "labelBlue";
+            this.labelBlue.Size = new System.Drawing.Size(14, 13);
+            this.labelBlue.TabIndex = 7;
+            this.labelBlue.Text = "B";
+            this.labelBlue.Click += new System.EventHandler(this.labelBlue_Click);
+            // 
+            // trackBarBlue
+            // 
+            this.trackBarBlue.Location = new System.Drawing.Point(128, 151);
+            this.trackBarBlue.Maximum = 255;
+            this.trackBarBlue.Name = "trackBarBlue";
+            this.trackBarBlue.Size = new System.Drawing.Size(104, 45);
+            this.trackBarBlue.TabIndex = 6;
+            this.trackBarBlue.Scroll += new System.EventHandler(this.trackBarBlue_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 572);
+            this.Controls.Add(this.numericUpDownBlue);
+            this.Controls.Add(this.labelBlue);
+            this.Controls.Add(this.trackBarBlue);
+            this.Controls.Add(this.numericUpDownGreen);
+            this.Controls.Add(this.labelGreen);
+            this.Controls.Add(this.trackBarGreen);
+            this.Controls.Add(this.numericUpDownRed);
+            this.Controls.Add(this.labelRed);
+            this.Controls.Add(this.trackBarRed);
+            this.Name = "Form1";
+            this.Text = "Keyboard UI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBarRed;
+        private System.Windows.Forms.Label labelRed;
+        private System.Windows.Forms.NumericUpDown numericUpDownRed;
+        private System.Windows.Forms.NumericUpDown numericUpDownGreen;
+        private System.Windows.Forms.Label labelGreen;
+        private System.Windows.Forms.TrackBar trackBarGreen;
+        private System.Windows.Forms.NumericUpDown numericUpDownBlue;
+        private System.Windows.Forms.Label labelBlue;
+        private System.Windows.Forms.TrackBar trackBarBlue;
+    }
+}
+
---
+++ Keyboard UI Csharp/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Keyboard_UI_Csharp
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BackColor = Color.DarkGray;
+            for (int x = 0; x < variables.selectedKeys.Length; x++)
+            {
+                variables.selectedKeys[0] = 0;
+            }
+            for (int x = 0; x < variables.keyRGBS.GetLength(0); x++)
+            {
+                variables.keyRGBS[x, 1] = 1;
+                variables.keyRGBS[x, 2] = 1;
+                variables.keyRGBS[x, 3] = 1;
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBarRed_Scroll(object sender, EventArgs e)
+        {
+            numericUpDownRed.Value = trackBarRed.Value;
+        }
+
+        private void numericUpDownRed_ValueChanged(object sender, EventArgs e)
+        {
+            trackBarRed.Value = decimal.ToInt32(numericUpDownRed.Value);
+        }
+
+        private void trackBarGreen_Scroll(object sender, EventArgs e)
+        {
+            numericUpDownGreen.Value = trackBarGreen.Value;
+        }
+
+        private void numericUpDownGreen_ValueChanged(object sender, EventArgs e)
+        {
+            trackBarGreen.Value = decimal.ToInt32(numericUpDownGreen.Value);
+        }
+
+        private void trackBarBlue_Scroll(object sender, EventArgs e)
+        {
+            numericUpDownBlue.Value = trackBarBlue.Value;
+        }
+
+        private void numericUpDownBlue_ValueChanged(object sender, EventArgs e)
+        {
+            trackBarBlue.Value = decimal.ToInt32(numericUpDownBlue.Value);
+        }
+
+        private void labelGreen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelBlue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void newColour(int one, int two, int three, int four)
+        {
+            Graphics g;
+            g = this.CreateGraphics();
+            variables.R = 1;
+            variables.G = 1;
+            variables.B = 1;
+            SolidBrush brushNewColour = new SolidBrush(Color.FromArgb(255, variables.R, variables.G, variables.B));
+            g.FillRectangle(brushNewColour, new Rectangle(one, two, three, four));
+            g.Dispose();
+
+        }
+
+        public void oldColour(int one, int two, int three, int four)
+        {
+            Graphics g;
+            g = this.CreateGraphics();
+            variables.oldR = 1;
+            variables.oldG = 1;
+            variables.oldB = 1;
+            SolidBrush brushOldColour = new SolidBrush(Color.FromArgb(255, variables.oldR, variables.oldG, variables.oldB));
+            g.FillRectangle(brushOldColour, new Rectangle(one, two, three, four));
+            g.Dispose();
+            int s;
+
+
+        }
+
+    }
+
+    public class variables
+    {
+        public static int R;
+        public static int G;
+        public static int B;
+        public static int oldR;
+        public static int oldG;
+        public static int oldB;
+        public static int keyNumber;
+
+        public static bool shiftPressed;
+        public static int[] selectedKeys = new int[121];
+        public static int[,] keyRGBS = new int[121, 4];
+
+
+    }
+}
